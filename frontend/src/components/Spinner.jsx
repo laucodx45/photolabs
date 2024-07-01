3c005cd977dfa1045c4cf9c4e1d68bdfe490be26
Cody <codylau6233@hotmail.com>
2024-06-30 21:34:18 -0700

    Connected front-end to cloud backend api. Add Loader component.

---
+++ frontend/src/components/Spinner.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ClipLoader } from 'react-spinners';
+import '../styles/Spinner.scss';
+
+const Loader = () => (
+  <>
+    <div className="loader-container">
+      <ClipLoader size={50} color={"#123abc"} loading={true} />
+    </div>
+    <p>The server is currently asleep but will spin up soon. Thank you for your patience!</p>
+  </>
+);
+
+export default Loader;
--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -3,16 +3,20 @@ import React, { useContext } from 'react';
 import PhotoList from 'components/PhotoList';
 import TopNavigation from 'components/TopNavigationBar';
 import PhotoDetailsModal from "routes/PhotoDetailsModal";
+import Loader from 'components/Spinner';
 import '../styles/HomeRoute.scss';
 import { applicationContext } from 'hooks/applicationContext';
 
 const HomeRoute = () => {
   const {state} = useContext(applicationContext);
   const modalState = state.modalState;
+  const photoData = state.photoData;
 
   return (
     <div className="home-route">
       <TopNavigation />
+      {/* if photoData length === 0, display spinner */}
+      {photoData.length === 0 && <Loader/>}
       <PhotoList />
       {modalState && <PhotoDetailsModal />}
     </div>
