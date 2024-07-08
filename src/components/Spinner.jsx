833709f8591a9a0b4ce4e2942976cadb179ca5e6
Cody <codylau6233@hotmail.com>
2024-07-07 22:25:25 -0700

    Change loading message

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -7,7 +7,7 @@ const Loader = () => (
     <div className="loader-container">
       <ClipLoader size={50} color={"#123abc"} loading={true} />
     </div>
-    <p className='loading-msg'>The server is currently asleep but will spin up soon. Thank you for your patience!</p>
+    <p className='loading-msg'>The server is currently asleep but will spin up soon. This may take up to 50 seconds. Thank you for your patience!</p>
   </>
 );
 
